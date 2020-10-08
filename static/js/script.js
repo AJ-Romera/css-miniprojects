a9bf997644b6bde6925d3646c94a2e17660b9f30
AJ-Romera <ajromeraalfonso@gmail.com>
2020-10-08 18:51:02 +0200

    Make the logic of miniproject 1
    - do two functions: AgeInDays that calculates your age in days and a reset to remove the result
    - use prompts to ask for your birth year and current year
    - buttons onclick to trigger the functions
    - access DOM to print the result in a H2 created through JS
    - centers, text-align and new width to the container-1

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,17 @@
-// alert('Hello world!');
-// console.log('hello');
+// Miniproject 1: Your Age in Days
+
+function ageInDays() {
+    alert('I´m going to tell you your age in days');
+    var birthYear = prompt('What year where you born... good friend?');
+    var currentYear = prompt('Tell me what year it´s right now please');
+    var ReturnAgeInDays = (currentYear - birthYear) * 365;
+    var h2 = document.createElement('h2');
+    var textAnswer = document.createTextNode('You are '+ReturnAgeInDays+' days young :D');
+    h2.setAttribute('id', 'ageInDays');
+    h2.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h2);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
