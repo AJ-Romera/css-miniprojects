9826938ccb61e97c3a747268095a6e9411a565b7
AJ-Romera <ajromeraalfonso@gmail.com>
2020-10-09 12:57:39 +0200

    Make a ramdom cat gif generator
    - Make containers, a button, etc in index.html
    - Style in style.css, just like the first container
    - Function in script.js attached to "generate cat" button so when you click it you get ramdom cats gifs.

--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,12 @@ function ageInDays() {
 function reset() {
     document.getElementById('ageInDays').remove();
 }
+
+// Miniproject 2: Cat Generator
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    image.alt = "Ramdom Cat Gif";
+    div.appendChild(image);
+}
